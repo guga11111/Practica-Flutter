c76cfdc1cac59666d9e3e69305369f2c8cd9c7f7
Gustavo <guga.nd@hotmail.com>
2021-03-03 18:10:17 -0600

    'secondbuttonadded'

--- lib/src/pages/conteo_page.dart
+++ lib/src/pages/conteo_page.dart
@@ -1,38 +1,58 @@
 import 'package:flutter/material.dart';
-class ConteoPage extends StatefulWidget{
+
+class ConteoPage extends StatefulWidget {
   @override
-  createState(){
+  createState() {
     return _ConteoPageState();
   }
 }
-class _ConteoPageState extends State<ConteoPage>{
-  final _estilo = new TextStyle(fontSize: 30);
-  int _conteo = 0;
+
+class _ConteoPageState extends State<ConteoPage> {
+  final _estilo = TextStyle(fontSize: 30);
+  int _conteo = 15;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Aqui va el título'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('clucks',style: _estilo),
-            Text('$_conteo',style: _estilo),
-          ],
+        appBar: AppBar(
+          title: Text('Titulo'),
+          centerTitle: true,
         ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: (){
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Clicks", style: _estilo),
+              Text("$_conteo", style: _estilo),
+            ],
+          ),
+        ),
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+                child: Icon(Icons.add),
+                elevation: 10,
+                onPressed: () {
+                  setState(() {
+                    _conteo++;
+                  });
+                }),
+            FloatingActionButton(
+                child: Icon(Icons.remove),
+                elevation: 1,
+                onPressed: () {
+                  setState(() {
+                    _conteo--;
+                  });
+                }),
+          ],
+        ));
+
+    /*child: Icon(Icons.add),
+        onPressed: () {
           setState(() {
             _conteo++;
           });
-        },
-      ),
-    );
+        },*/
   }
 }
